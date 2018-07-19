0324d57f297bf20acf6e731162b5c0cda0bbbe4f
Balakrishna Muthyala <Balakrishna.M@allfon.in>
2018-07-19 18:54:26 +0530

    19july2018

--- src/test/java/sampleTestPackage/SampleTestClass.java
+++ src/test/java/sampleTestPackage/SampleTestClass.java
@@ -10,22 +10,44 @@ import frameworkPackage.FrameworkBase;
 public class SampleTestClass extends FrameworkBase
 {
 
-	public static void main(String args[])
+	public static void main(String args[]) throws InterruptedException
 	{
-		
-		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		String baseUrl = "http://demo.guru99.com/test/upload/";
+		/*
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);		
 		WebDriver driver = new ChromeDriver();
+		
+		String baseUrl = "http://demo.guru99.com/test/upload/";
 		driver.get(baseUrl);
 		
+		//Files direct uploading without using AutoIt or Robot classes
         WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
         uploadElement.sendKeys("D:\\new.txt");
-
+		Thread.sleep(2000);
+		
         driver.findElement(By.id("terms")).click();
-
+		Thread.sleep(2000);
+		
         // click the "UploadFile" button
         driver.findElement(By.name("send")).click();
-				
+		*/		
+		
+		
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);		
+		WebDriver driver = new ChromeDriver();
+		
+		String baseUrl = "https://files.fm/";
+		driver.get(baseUrl);
+		
+		//Files direct uploading without using AutoIt or Robot classes
+        WebElement uploadElement = driver.findElement(By.xpath("//input[@id='file_upload']/following-sibling::input"));
+        uploadElement.sendKeys("D:\\new.txt");
+        Thread.sleep(2000);
+                       
+        driver.findElement(By.xpath("//div[contains(text(),'Start file upload')]")).click();
+        Thread.sleep(2000);
+
 	}
 	
 }
+
+
